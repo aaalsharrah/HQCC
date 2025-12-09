ff8bf2f9d2e366f2c0caa173dfab2e09508dbaf2
Subhan2117 <Subhannadeem06@gmail.com>
2025-12-08 23:08:52 -0500

    fixed 404

--- app/(admin)/admin/events/[id]/page.jsx
+++ app/(admin)/admin/events/[id]/page.jsx
@@ -2,14 +2,6 @@
 
 import AdminEventRegistrationsClient from './AdminEventRegistrationsClient';
 
-// For static export: tell Next not to generate any specific IDs
-export function generateStaticParams() {
-  return [];
-}
-
-// Optional: make it explicit that we don't support arbitrary dynamic params
-export const dynamicParams = false;
-
 export default function AdminEventRegistrationsPage(props) {
   return <AdminEventRegistrationsClient {...props} />;
 }
